b7d1caeaab877dfe22f28fa94d00a954353e2a07
Juan David Molina Ruano <mr24004@esfe.agape.edu.sv>
2024-05-23 08:07:32 -0600

    Se agregaron propiedades a la clase Empleado

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,3 +1,44 @@
+import java.time.LocalDate;
+
 public class Empleado {
 
+    private String nombre;
+    private double sueldo;
+    private LocalDate fechaPago;
+    
+    public Empleado() {
+    }
+
+    public Empleado(String nombre, double sueldo, LocalDate fechaPago) {
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+        this.fechaPago = fechaPago;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public LocalDate getFechaPago() {
+        return fechaPago;
+    }
+
+    public void setFechaPago(LocalDate fechaPago) {
+        this.fechaPago = fechaPago;
+    }
+
+    
+    
 }
